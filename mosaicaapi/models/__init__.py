2a84131589d1458b2d4fc582039e83f6a02ae188
Patrick Burns <burns212@gmail.com>
2023-01-21 14:12:18 -0600

    some fixtures

---
+++ mosaicaapi/models/__init__.py
@@ -0,0 +1,6 @@
+from .badQuestionReport import BadQuestionReport
+from .question import Question
+from .responseSet import ResponseSet
+from .tile import Tile
+from .user import User
+from .userTileScore import UserTileScore
--- mosaicaapi/models/user.py
+++ mosaicaapi/models/user.py
@@ -4,7 +4,3 @@ from django.db import models
 class User(models.Model):
 
     uid = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.name
-    
