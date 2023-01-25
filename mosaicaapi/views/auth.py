5999da7a58bc5c3387c07c042dbfb6432f6c748d
Patrick Burns <burns212@gmail.com>
2023-01-24 20:23:16 -0600

    got some auth and user views and urls

---
+++ mosaicaapi/views/auth.py
@@ -0,0 +1,51 @@
+from mosaicaapi.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['POST'])
+def check_user(request):
+    '''Checks to see if User has Associated user
+
+    Method arguments:
+      request -- The full HTTP request object
+    '''
+    uid = request.data['uid']
+
+    # Use the built-in authenticate method to verify
+    # authenticate returns the user object or None if no user is found
+    try:
+        user = User.objects.get(uid=uid)
+
+    # If authentication was successful, respond with their token
+        data = {
+            'id': user.id,
+            'uid': user.uid,
+        }
+        print(data)
+        return Response(data)
+    except ValueError:
+        # Bad login details were provided. So we can't log the user in.
+        data = { 'valid': False }
+        return Response(data)
+
+@api_view(['POST'])
+def register_user(request):
+    '''Handles the creation of a new user for authentication
+
+    Method arguments:
+      request -- The full HTTP request object
+    '''
+
+    # Now save the user info in the rarev2api_user table
+    user = User.objects.create(
+        uid=request.data['uid'],
+    )
+
+    # Return the user info to the client
+    data = {
+            'id': user.id,
+            'uid': user.uid,
+    }
+    print(data)
+    return Response(data)
